d5e4bd5f8e3aeaa1e007fa8427a8b38c431abb90
Arthur <costa.arthur.lima@gmail.com>
2023-12-19 14:42:45 -0300

    inimigo só atira qnd ve o jogador e pode mirar pra cima

--- Assets/Scripts/EnemyScripts/Torret.cs
+++ Assets/Scripts/EnemyScripts/Torret.cs
@@ -8,6 +8,8 @@ public class Torret : MonoBehaviour
     [SerializeField] private float cannonForce;
     [SerializeField] private float fireCooldown = 4f;
     [SerializeField] private Transform firePoint;
+    [SerializeField] private LayerMask PlayerMask;
+
 
     private float timeSinceLastFire = 0f;
     private Transform target;
@@ -19,16 +21,25 @@ public class Torret : MonoBehaviour
 
     void Update()
     {
+        timeSinceLastFire += Time.deltaTime;
         Vector3 targetPos = target.position;
-        targetPos.y = transform.position.y;
+        if (targetPos.y < transform.position.y)
+            targetPos.y = transform.position.y;
 
         transform.LookAt(targetPos);
 
-        // fire
-        timeSinceLastFire += Time.deltaTime;
-        if (timeSinceLastFire > fireCooldown) {
-            Fire();
-            timeSinceLastFire = 0f;
+        RaycastHit hit;
+        Vector3 directionToTarget = (targetPos - transform.position).normalized;
+        if (Physics.Raycast(transform.position, directionToTarget, out hit)) {
+            Debug.DrawLine(transform.position, hit.point, Color.yellow, 0.0f);
+            if (hit.collider.CompareTag("Player"))
+            {
+                // fire
+                if (timeSinceLastFire > fireCooldown) {
+                    Fire();
+                    timeSinceLastFire = 0f;
+                }
+            }   
         }
     }
 
@@ -38,3 +49,4 @@ public class Torret : MonoBehaviour
         newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * cannonForce, ForceMode.Impulse);
     }
 }
+
